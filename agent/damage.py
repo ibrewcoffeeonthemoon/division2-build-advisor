f0414452fe9799a3e42ab72231b48578292665f0
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-08-21 22:21:49 +0800

    show base damage

--- agent/damage.py
+++ agent/damage.py
@@ -140,7 +140,7 @@ class _ComputeGraphManager(ABC):
             return f' x (1 + {content})\n'
 
         t = 'Breakdown:\n'
-        t += 'DMG = BaseDamage\n'
+        t += f'DMG = BaseDamage({self._base_dmg.item():,.0f})\n'
         t += presenting(WD)
         t += presenting(TWD)
         t += presenting(AMP1)
