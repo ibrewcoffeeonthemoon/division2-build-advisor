5e5444b0f0111db58a6843b7692be650d5ea96e8
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-08-22 00:52:33 +0800

    Lexington AR

--- agent/item/weapon.py
+++ agent/item/weapon.py
@@ -16,11 +16,26 @@ class Weapon(Item):
         self.rpm = rpm
 
 
-# class Lexington(Weapon):
-#     def __init__(self) -> None:
-#         super().__init__(
-#             'Lexington',
-#
-#             base_damage=48_700,
-#             rpm=850
-#         )
+class Lexington(Weapon):
+    def __init__(
+        self,
+        *extras: _Attribute,
+        expertise: int = 0,
+        ar_damage: float = 0.15,
+        dth: float = 0.21,
+        dth_uptime: float = 0.3,
+        attributes: _Attribute | None = None,
+    ) -> None:
+        self.core_attr1 = WD(ar_damage, name='ARType')
+        self.core_attr2 = DTH(dth, uptime=dth_uptime)
+        self.attr = DTTOOC(0.10, uptime=0.9) if attributes is None else attributes
+        super().__init__(
+            WD(expertise*0.01, name=f'Expertise.{expertise}'),
+            self.core_attr1,
+            self.core_attr2,
+            self.attr,
+            *extras,
+            base_damage=48_700,
+            rpm=850,
+            name='Lexington',
+        )
