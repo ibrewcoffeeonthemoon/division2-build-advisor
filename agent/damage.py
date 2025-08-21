3953c521604f38a84caa85d3679e8cc3c7e2ce46
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-08-21 19:29:25 +0800

    get intermediate tensors ready to create the compute graph

--- agent/damage.py
+++ agent/damage.py
@@ -1,3 +1,7 @@
+from torch import Tensor, tensor
+
+from agent.item.attribute import *
+from agent.item.attribute import _DTA_DTH
 from agent.item.gear import Gears
 from agent.item.specialization import Specialization
 from agent.item.watch import KeenersWatch
@@ -19,6 +23,48 @@ class _ComputeGraphManager:
         self._weapon = weapon
         self._gears = gears
         self._extras = extras
+
+        # compute graph
+
+        def accumulate(T: type, init_val: float = 0.0) -> Tensor:
+            val = tensor(init_val)
+            # weapon
+            for a in weapon.attributes:
+                if isinstance(a, T):
+                    val += a.expected_value
+
+            # inventory items
+            for items in (gears, extras):
+                for item in items:
+                    for a in item.attributes:
+                        if isinstance(a, T):
+                            val += a.expected_value
+
+            #
+            return val
+
+        self._wd = tensor(1.0) + accumulate(WD)
+        self._twd = tensor(1.0) + accumulate(TWD)
+        self._amp1 = tensor(1.0) + accumulate(AMP1)
+        self._amp2 = tensor(1.0) + accumulate(AMP2)
+        self._amp3 = tensor(1.0) + accumulate(AMP3)
+        self._chc = accumulate(CHC, chc_basic)
+        self._chd = accumulate(CHD, chd_basic)
+        self._hs = accumulate(HS, hs_basic)
+        self._hsc = tensor(hsc_basic)
+        self._crit_hs = tensor(1.0) + self._chc * self._chd + self._hs * self._hsc
+        self._dta_dth = tensor(1.0) + accumulate(_DTA_DTH)
+        self._dttooc = tensor(1.0) + accumulate(DTTOOC)
+        self._dmg_x = (
+            self._wd *
+            self._twd *
+            self._amp1 *
+            self._amp2 *
+            self._amp3 *
+            self._crit_hs *
+            self._dta_dth *
+            self._dttooc
+        )
 
 
 class DMGx(_ComputeGraphManager):
