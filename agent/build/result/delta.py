154cf8ff14233dbe74eee6da9472637197239ec8
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-08-23 03:12:40 +0800

    formatting

--- agent/build/result/delta.py
+++ agent/build/result/delta.py
@@ -8,16 +8,25 @@ class Delta(_ResultHandler):
     def __call__(self) -> None:
         def text(m: _ComputeGraphManager) -> str:
             g = m.gradients
-            t = f'{g.name} Delta:\n'
+            line = '<line />\n'
+
+            t = line
+            t += f'{g.name} Delta:\n'
             for items in g.items_ls:
                 for item in items.items:
+                    t += line
                     t += f'{" "*2}{item.name}:\n'
                     for attr in item.attrs:
                         if attr.grad is not None:
                             name = f'{attr.name} {attr.value:.1%}'
                             delta = attr.value * attr.grad
                             t += f'{" "*4}{name:20}: {delta:{g.grad_format}}\n'
-            return t
+            t += line
+
+            return t.replace(
+                '<line />',
+                '-'*max(len(s) for s in t.splitlines())
+            )
 
         txt = merge_text_side_by_side(
             text(self._managers[0]),
