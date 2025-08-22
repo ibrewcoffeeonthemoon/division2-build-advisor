e4c66669057e331b75064bda6feb9ba193544f70
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-08-23 03:28:22 +0800

    formatting

--- agent/build/result/breakdown.py
+++ agent/build/result/breakdown.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from agent.build.damage import _ComputeGraphManager, Output
 from agent.build.result._handler import _ResultHandler
+from agent.utils import merge_text_side_by_side
 
 
 class Breakdown(_ResultHandler):
@@ -8,38 +9,46 @@ class Breakdown(_ResultHandler):
         def text(m: _ComputeGraphManager) -> str:
             b = m.breakdown
             d = b.data
+            line = '<line />\n'
 
-            def joining(ls: list[Output.Breakdown.Data.Attribute]) -> str:
-                return ' + '.join([
+            def joining(ls: list[Output.Breakdown.Data.Attribute], char: str) -> str:
+                return char.join([
                     f'{a.name} {a.expected_value:.1%}'
                     for a in ls])
 
-            def presenting(ls: list[Output.Breakdown.Data.Attribute]) -> str:
-                content = joining(ls)
+            def presenting(ls: list[Output.Breakdown.Data.Attribute], char: str = '\n      + ') -> str:
+                content = joining(ls, char)
                 if len(content) == 0:
                     return ''
                 return f' x (1 + {content})\n'
 
-            t = 'Breakdown:\n'
+            t = line
+            t += 'Breakdown:\n'
+            t += line
             t += f'{b.weapon_name}:\n'
-            t += f'DMG {d.DMG:,.0f} = BaseDamage {d.BaseDamage:,.0f}\n'
+            t += line
+            t += f' BaseDamage {d.BaseDamage:,.0f}\n'
             t += presenting(d.WD)
             t += presenting(d.TWD)
             t += presenting(d.AMP1)
             t += presenting(d.AMP2)
             t += presenting(d.AMP3)
-            t += (
-                f' x (1 + CHC {d.CHC:.1%} x CHD {d.CHD:.1%} '
-                f'+ HS {d.HS:.1%} x HSC {d.HSC:.1%})\n'
-            )
+            t += f' x (1 + CHC {d.CHC:.1%} x CHD {d.CHD:.1%}\n'
+            t += f'      + HS {d.HS:.1%} x HSC {d.HSC:.1%})\n'
             t += presenting(d._DTA_DTH)
             t += presenting(d.DTTOOC)
+            t += line
+            t += f' = DMG {d.DMG:,.0f}\n'
+            t += line
 
-            return t
+            return t.replace(
+                '<line />',
+                '-'*max(len(s) for s in t.splitlines())
+            )
 
-        txt = '\n'.join([
+        txt = merge_text_side_by_side(
             text(self._managers[0]),
             text(self._managers[1]),
-        ])
+        )
 
         print(txt)
