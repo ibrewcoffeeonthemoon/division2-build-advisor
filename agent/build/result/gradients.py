e612da54c26173890e3d5d9cec7dda7acf6d9e83
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-08-23 03:11:03 +0800

    formatting

--- agent/build/result/gradients.py
+++ agent/build/result/gradients.py
@@ -8,15 +8,24 @@ class Gradients(_ResultHandler):
     def __call__(self) -> None:
         def text(m: _ComputeGraphManager) -> str:
             g = m.gradients
-            t = f'{g.name} Gradients:\n'
+            line = '<line />\n'
+
+            t = line
+            t += f'{g.name} Gradients:\n'
             for items in g.items_ls:
                 for item in items.items:
+                    t += line
                     t += f'{" "*2}{item.name}:\n'
                     for attr in item.attrs:
                         if attr.grad is not None:
                             name = f'{attr.name} {attr.value:.1%}'
                             t += f'{" "*4}{name:20}: {attr.grad:{g.grad_format}}\n'
-            return t
+            t += line
+
+            return t.replace(
+                '<line />',
+                '-'*max(len(s) for s in t.splitlines())
+            )
 
         txt = merge_text_side_by_side(
             text(self._managers[0]),
