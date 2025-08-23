8b70a11bc2a12e179278495538df405ffab1badd
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-08-23 22:30:39 +0800

    gradients handler abc

--- agent/build/result/gradients/__init__.py
+++ agent/build/result/gradients/__init__.py
@@ -1,5 +1,41 @@
+from abc import ABC, abstractmethod
 from agent.build.result._handler import _ResultHandler
+import agent.build.damage.output as Output
 
 
-class _GradientsHandler(_ResultHandler):
-    pass
+class _GradientsHandler(_ResultHandler, ABC):
+    @property
+    @abstractmethod
+    def _topic(self) -> str: ...
+
+    @abstractmethod
+    def _item_row_text(
+        self,
+        attr: Output.Gradients.Items.Item.Attribute,
+        grad_format: str,
+    ) -> str: ...
+
+    def text(self, weapon_id: int = 0) -> str:
+        m = self._managers[weapon_id]
+        g = m.gradients
+
+        t = self.SEP
+        t += f'{g.name} {self._topic}:\n'
+        t += self.SEP
+        t += f'Build: {self._build.name}\n'
+        for items in g.items_ls:
+            for item in items.items:
+                t += self.SEP
+                t += f'{item.type}: {item.name}\n'
+                for attr in item.attrs:
+                    if attr.grad is not None:
+                        # name = f'{attr.name} {attr.value:.1%}'
+                        # delta = attr.value * attr.grad
+                        # t += f'  {name:20}: {delta:{g.grad_format}}\n'
+                        t += self._item_row_text(attr, g.grad_format)
+        t += self.SEP
+
+        return t.replace(
+            self.SEP_TAG,
+            '-'*max(len(s) for s in t.splitlines())
+        )
