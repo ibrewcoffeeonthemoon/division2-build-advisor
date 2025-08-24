f7450002854cbd4d6bde03a4a20d0ce12e3a2023
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-08-24 15:03:54 +0800

    simplify

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from agent import Build
 from agent.item.attribute import *
 from agent.item.gear import *
 from agent.item.specialization import *
-from agent.item.watch import KeenersWatch
+from agent.item.watch import *
 from agent.item.weapon import *
 
 
