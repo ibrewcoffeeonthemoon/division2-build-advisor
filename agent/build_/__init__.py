fec7a6c3decd5e93a5504869696ab061f68832a1
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-08-27 15:12:35 +0800

    move out gears

--- agent/build_/__init__.py
+++ agent/build_/__init__.py
@@ -2,11 +2,11 @@ from copy import deepcopy
 from functools import cache, cached_property
 from typing import Self, Type, TypeVar
 
+from agent.build_.gears import _Gears
 from agent.build_.weapons import _Weapons
 from agent.damage import DMG, DPS, DMGx, DPSx, _ComputeGraphManager
 from agent.item.attribute import *
-from agent.item.gear import (Backpack, Chest, Gears, Gloves, Holster, Kneepads,
-                             Mask)
+from agent.item.gear import Backpack, Chest, Gloves, Holster, Kneepads, Mask
 from agent.item.specialization import Specialization
 from agent.item.watch import KeenersWatch
 from agent.item.weapon import Weapon
@@ -15,72 +15,6 @@ from agent.result._handler import _ResultHandler
 from agent.utils import merge_text_side_by_side
 
 T = TypeVar('T', bound=_ComputeGraphManager)
-
-
-class _Gears:
-    def __init__(self) -> None:
-        self._mask: Mask
-        self._backpack: Backpack
-        self._chest: Chest
-        self._gloves: Gloves
-        self._holster: Holster
-        self._kneepads: Kneepads
-
-    @property
-    def mask(self) -> Mask:
-        return self._mask
-
-    @mask.setter
-    def mask(self, mask: Mask) -> None:
-        self._mask = mask
-
-    @property
-    def backpack(self) -> Backpack:
-        return self._backpack
-
-    @backpack.setter
-    def backpack(self, backpack: Backpack) -> None:
-        self._backpack = backpack
-
-    @property
-    def chest(self) -> Chest:
-        return self._chest
-
-    @chest.setter
-    def chest(self, chest: Chest) -> None:
-        self._chest = chest
-
-    @property
-    def gloves(self) -> Gloves:
-        return self._gloves
-
-    @gloves.setter
-    def gloves(self, gloves: Gloves) -> None:
-        self._gloves = gloves
-
-    @property
-    def holster(self) -> Holster:
-        return self._holster
-
-    @holster.setter
-    def holster(self, holster: Holster) -> None:
-        self._holster = holster
-
-    @property
-    def kneepads(self) -> Kneepads:
-        return self._kneepads
-
-    @kneepads.setter
-    def kneepads(self, kneepads: Kneepads) -> None:
-        self._kneepads = kneepads
-
-    @property
-    def _gears(self) -> Gears:
-        return (
-            self._mask, self._backpack,
-            self._chest, self._gloves,
-            self._holster, self._kneepads,
-        )
 
 
 class _Extras:
