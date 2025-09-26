3740ebcff9382a6db94a1fc83557867f564e9b4a
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-09-26 10:24:27 +0800

    refactor into components

---
+++ src/components/Main.tsx
@@ -0,0 +1,22 @@
+function Main() {
+  return (
+    <main className="flex-1 flex flex-col" id="main">
+      <div className="flex-1 flex flex-col text-center">
+        {/* Title */}
+        <h1 className="text-4xl p-4">Build</h1>
+        {/* Content */}
+        <div
+          className="flex-1 flex flex-col justify-around items-stretch"
+          id="content"
+        >
+          <p className="">Content Content Content Content</p>
+          <p className="">Content Content Content Content</p>
+          <p className="">Content Content Content Content</p>
+          <p className="">Content Content Content Content</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
