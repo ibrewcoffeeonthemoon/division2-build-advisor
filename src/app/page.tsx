3420814512b4baa394404073dc04cf74867f31ac
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-09-26 10:38:14 +0800

    added Top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@
 
 import Main from "@/components/Main";
 import Bottom from "@/components/Bottom";
+import Top from "@/components/Top";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
+      <Top />
       <Main />
       <Bottom />
     </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,14 @@
 function Main() {
   return (
     <main className="flex-1 flex flex-col" id="main">
-      <div className="flex-1 flex flex-col text-center">
-        {/* Title */}
-        <h1 className="text-4xl p-4">Build</h1>
-        {/* Content */}
-        <div
-          className="flex-1 flex flex-col justify-around items-stretch"
-          id="content"
-        >
-          <p className="">Content Content Content Content</p>
-          <p className="">Content Content Content Content</p>
-          <p className="">Content Content Content Content</p>
-          <p className="">Content Content Content Content</p>
-        </div>
+      <div
+        className="flex-1 flex flex-col justify-around items-stretch text-center"
+        id="content"
+      >
+        <p className="">Content Content Content Content</p>
+        <p className="">Content Content Content Content</p>
+        <p className="">Content Content Content Content</p>
+        <p className="">Content Content Content Content</p>
       </div>
     </main>
   );
