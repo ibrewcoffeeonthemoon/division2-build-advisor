3c45430afe02f8a7d5eb49b00fc3d8fe49cd1f60
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-09-27 07:29:20 +0800

    Gear Slot

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,32 @@
+const Slot = ({ name }: { name: string }) => {
+  return (
+    <div
+      className={`
+        h-40 py-2 px-1 m-2
+        rounded-2xl
+        bg-orange-400 text-2xl font-bold
+      `}
+    >
+      {name}
+    </div>
+  );
+};
+
 export default function Main() {
   return (
-    <div className="flex-1 flex flex-col justify-center">
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
-      <p>4</p>
-      <p>5</p>
+    <div
+      className={`
+        flex-1
+        grid grid-cols-2 items-center content-start
+        overflow-auto
+      `}
+    >
+      <Slot name="Mask" />
+      <Slot name="Backpack" />
+      <Slot name="Chest" />
+      <Slot name="Gloves" />
+      <Slot name="Holster" />
+      <Slot name="Kneepads" />
     </div>
   );
 }
