fd7b31378267ae1ea77012e8ff572dc09cd3bc67
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-09-29 13:22:27 +0800

    Button with Icons

--- src/components/Bottom.tsx
+++ src/components/Bottom.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import { ChartBarIcon } from "@heroicons/react/24/solid";
+
 const Button = ({ name, target }: { name: string; target: string }) => {
   return (
-    <a
-      href={target}
-      className="
-        text-sm font-bold hover:text-gray-300 
+    <div className="flex flex-col items-center">
+      <ChartBarIcon className="w-8 h-8" />
+      <a
+        href={target}
+        className="
+        text-sm hover:text-gray-300 
         transition-colors duration-300
       "
-    >
-      {name}
-    </a>
+      >
+        {name}
+      </a>
+    </div>
   );
 };
 
