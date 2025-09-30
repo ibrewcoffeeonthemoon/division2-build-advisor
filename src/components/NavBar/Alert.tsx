9e5342b5262f15ce2d27fc3bc72b29c4d2b6f136
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-09-30 11:41:03 +0800

    apply to Alert

--- src/components/NavBar/Alert.tsx
+++ src/components/NavBar/Alert.tsx
@@ -5,7 +5,8 @@ import { InformationCircleIcon } from "@heroicons/react/24/solid";
 import { useEffect } from "react";
 
 export const Alert = () => {
-  const { currentUrl, setCurrentUrl } = store.app.useStore();
+  const currentUrl = store.app.currentUrl();
+  const setCurrentUrl = store.app.setCurrentUrl();
   const showAlert = store.ui.NavBar.showAlert();
   const setShowAlert = store.ui.NavBar.setShowAlert();
 
