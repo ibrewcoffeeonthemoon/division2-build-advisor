ef2f839cb273eef20622357875e841c6a22a85c2
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-09-30 11:38:06 +0800

    apply to index

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,9 @@
-import { useStore as ui_NavBar } from "@/store/ui/NavBar";
+import ui_NavBar from "@/store/ui/NavBar";
 import { useStore as app } from "@/store/app";
 
 export const store = {
   ui: {
-    NavBar: { useStore: ui_NavBar },
+    NavBar: ui_NavBar,
   },
   app: {
     useStore: app,
