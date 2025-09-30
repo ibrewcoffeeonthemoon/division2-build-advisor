178e2faa07c552bb4ed8d38642bee70bd63f521f
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-09-30 14:42:22 +0800

    this will already display a drawer

--- src/components/NavBar/DrawerToggleButton.tsx
+++ src/components/NavBar/DrawerToggleButton.tsx
@@ -1,5 +1,31 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
 
 export default function DrawerToggleButton() {
-  return <Bars3Icon className="h-7 w-7" />;
+  return (
+    <div className="drawer">
+      <input id="drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        {/* Page content here */}
+        <label htmlFor="drawer" className="drawer-button">
+          <Bars3Icon className="h-7 w-7" />
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="drawer"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+            <a>Sidebar Item 1</a>
+          </li>
+          <li>
+            <a>Sidebar Item 2</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
