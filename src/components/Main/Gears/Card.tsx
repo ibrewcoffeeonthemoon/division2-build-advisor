cebc19741c371744ecbdf2f5518ff41c422100af
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-01 20:24:01 +0800

    apply Gear

--- src/components/Main/Gears/Card.tsx
+++ src/components/Main/Gears/Card.tsx
@@ -1,10 +1,11 @@
 import { ItemCard } from "../lib/Card";
 
 type Props = {
+  category: string;
   name: string;
 };
 
-export default function Card({ name }: Props) {
+export default function Card({ category, name }: Props) {
   return (
     <ItemCard
       itemAttrs={
@@ -20,7 +21,12 @@ export default function Card({ name }: Props) {
           Talents HS 6%
         </p>
       }
-      itemDesc={<h2 className="card-title">{name}</h2>}
+      itemDesc={
+        <div className="text-right">
+          <h2 className="card-title">{category}</h2>
+          <h2>{name}</h2>
+        </div>
+      }
     />
   );
 }
