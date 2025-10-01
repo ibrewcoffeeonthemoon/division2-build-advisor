ebbd0d59e36ae5128307568476e933227d9e19b5
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-02 06:50:12 +0800

    include more weapons

--- src/components/Main/Weapons/index.tsx
+++ src/components/Main/Weapons/index.tsx
@@ -19,8 +19,10 @@ export default function Weapons() {
         />
       }
     >
-      <Card category="Weapon1" name="name" />
-      <Card category="Weapon2" name="name" />
+      <Card category="Primary" name="name" />
+      <Card category="Secondary" name="name" />
+      <Card category="Sidearm" name="name" />
+      <Card category="Signature" name="name" />
     </Section>
   );
 }
