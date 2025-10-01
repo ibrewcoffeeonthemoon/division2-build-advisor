a79b28a0c6a2efff8a9403920c227ad501037fce
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-01 11:29:50 +0800

    cleanup

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,33 +2,6 @@ import { ReactNode } from "react";
 import GearCard from "./GearCard";
 import WeaponCard from "./WeaponCard";
 
-const Slot = ({ name }: { name: string }) => {
-  return (
-    <div
-      className="
-        card card-sm bg-accent/20 shadow-sm
-        m-1
-        text-xl font-bold
-      "
-    >
-      <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>
-          Core Attributes: WD 15%
-          <br />
-          Minor Attributes CHD 15%
-          <br />
-          Minor Attributes CHC 6%
-          <br />
-          Mods HS 6%
-          <br />
-          Talents HS 6%
-        </p>
-      </div>
-    </div>
-  );
-};
-
 const Section = ({ name, children }: { name: string; children: ReactNode }) => {
   return (
     <div className="card card-sm bg-base-300 shadow-sm">
