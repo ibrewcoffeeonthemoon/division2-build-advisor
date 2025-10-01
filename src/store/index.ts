919c59f91ff88e3fe9938a9779cedfa9dd37c994
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-01 13:32:03 +0800

    create Main/Weapons Main/Gears store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,16 @@
 import NavBar from "@/store/ui/NavBar";
+import Weapons from "@/store/ui/Main/Weapons";
+import Gears from "@/store/ui/Main/Gears";
 import Dock from "@/store/ui/Dock";
 import app from "@/store/app";
 
 export const store = {
   ui: {
     NavBar,
+    Main: {
+      Weapons,
+      Gears,
+    },
     Dock,
   },
   app,
