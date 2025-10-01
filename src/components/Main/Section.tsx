57bd50603994c5998fc4d6bd218d876257361dc3
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-01 13:32:34 +0800

    apply controller to Comps

--- src/components/Main/Section.tsx
+++ src/components/Main/Section.tsx
@@ -1,19 +1,20 @@
+"use client";
 import { ReactNode } from "react";
 
 export const Section = ({
   name,
+  control,
   children,
 }: {
   name: string;
+  control: ReactNode;
   children: ReactNode;
 }) => {
   return (
-    <details
-      className="collapse collapse-arrow bg-base-100 border-base-300 border"
-      open
-    >
-      <summary className="collapse-title font-semibold">{name}</summary>
+    <div className="collapse collapse-arrow bg-base-100 border-base-300 border">
+      {control}
+      <div className="collapse-title font-semibold">{name}</div>
       <div className="collapse-content text-sm">{children}</div>
-    </details>
+    </div>
   );
 };
--- src/components/Main/Weapons/index.tsx
+++ src/components/Main/Weapons/index.tsx
@@ -1,9 +1,24 @@
+"use client";
+
 import { Section } from "../Section";
 import Card from "./Card";
+import { store } from "@/store";
 
 export default function Weapons() {
+  const collapseOpen = store.ui.Main.Weapons.collapseOpen();
+  const setCollapseOpen = store.ui.Main.Weapons.setCollapseOpen();
+
   return (
-    <Section name="Weapons">
+    <Section
+      name="Weapons"
+      control={
+        <input
+          type="checkbox"
+          checked={collapseOpen}
+          onChange={(e) => setCollapseOpen(e.currentTarget.checked)}
+        />
+      }
+    >
       <Card name="Weapon1" />
       <Card name="Weapon2" />
     </Section>
