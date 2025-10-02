88d1e93bc2b8037a60f5fc6ef1724471a88399cf
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-02 19:30:04 +0800

    refactor out NameInput Input

--- src/components/Main/lib/Editor/index.tsx
+++ src/components/Main/lib/Editor/index.tsx
@@ -1,17 +1,4 @@
-export const NameInput = () => {
-  return (
-    <label className="input input-neutral w-full col-span-12">
-      <span className="font-bold">Name</span>
-      <input
-        type="text"
-        className="grow text-primary font-bold pl-1"
-        placeholder=">>>"
-        onFocus={(e) => e.currentTarget.select()}
-      />
-      <span className="badge badge-ghost badge-xs">Required</span>
-    </label>
-  );
-};
+import { Input, NameInput } from "./Input";
 
 export const Header = () => {
   const headers = [
@@ -31,47 +18,6 @@ export const Header = () => {
         </span>
       ))}
     </>
-  );
-};
-
-const Input = () => {
-  return (
-    <div className="col-span-12 grid grid-cols-12 items-center">
-      <input
-        className="input input-ghost col-span-4"
-        type="text"
-        placeholder="Name"
-        list="common-names"
-      />
-      <datalist id="common-names">
-        <option value="RedCore"></option>
-        <option value="Firefox"></option>
-        <option value="Safari"></option>
-        <option value="Opera"></option>
-        <option value="Edge"></option>
-      </datalist>
-      <select
-        className="select select-ghost w-auto col-span-4"
-        defaultValue="WD"
-      >
-        <option disabled={true}>Attribute</option>
-        <option>CHC</option>
-        <option>CHD</option>
-        <option>HS</option>
-      </select>
-
-      <label className="grow input input-ghost input-md w-full items-center col-span-2">
-        <input type="text" className="grow" placeholder="Value" />
-      </label>
-      <label className="grow input input-ghost input-md w-full items-center col-span-2">
-        <input
-          type="text"
-          className="grow"
-          placeholder="Uptime"
-          defaultValue={1.0}
-        />
-      </label>
-    </div>
   );
 };
 
