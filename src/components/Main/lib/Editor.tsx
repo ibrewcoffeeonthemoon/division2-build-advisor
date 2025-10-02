35d6ed72f618fca653789978306db481743c1dea
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-02 13:51:52 +0800

    NameInput

--- src/components/Main/lib/Editor.tsx
+++ src/components/Main/lib/Editor.tsx
@@ -5,6 +5,21 @@ import {
   CubeIcon,
 } from "@heroicons/react/24/solid";
 
+export const NameInput = () => {
+  return (
+    <label className="input input-neutral w-full col-span-12">
+      <span className="font-bold">Name</span>
+      <input
+        type="text"
+        className="grow text-primary font-bold pl-1"
+        placeholder=">>>"
+        onFocus={(e) => e.currentTarget.select()}
+      />
+      <span className="badge badge-ghost badge-xs">Required</span>
+    </label>
+  );
+};
+
 const Input = ({
   icon,
   field,
@@ -15,7 +30,7 @@ const Input = ({
   placeholder: string;
 }) => {
   return (
-    <div className="grid grid-cols-12 items-center">
+    <div className="col-span-12 grid grid-cols-12 items-center">
       <div className="col-span-1">{icon}</div>
       <select
         className="select select-ghost w-auto col-span-2"
@@ -46,7 +61,8 @@ const Input = ({
 
 export const Editor = () => {
   return (
-    <div className="collapse-content px-3">
+    <div className="grid grid-cols-12 collapse-content px-3">
+      <NameInput />
       <Input
         icon={<StopCircleIcon className="text-red-500 size-7" />}
         field="Name"
