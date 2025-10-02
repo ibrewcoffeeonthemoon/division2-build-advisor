b61b3672fcd2916cb427d32fb326ed995335a74e
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-02 19:05:52 +0800

    simplify the editor input

--- src/components/Main/lib/Editor.tsx
+++ src/components/Main/lib/Editor.tsx
@@ -1,10 +1,3 @@
-import { ReactNode } from "react";
-import {
-  StopCircleIcon,
-  Cog8ToothIcon,
-  CubeIcon,
-} from "@heroicons/react/24/solid";
-
 export const NameInput = () => {
   return (
     <label className="input input-neutral w-full col-span-12">
@@ -20,20 +13,24 @@ export const NameInput = () => {
   );
 };
 
-const Input = ({
-  icon,
-  field,
-  placeholder,
-}: {
-  icon: ReactNode;
-  field: string;
-  placeholder: string;
-}) => {
+export const Header = () => {
+  const headers = ["Amplifier", "Attribute", "Value", "Uptime"];
+  return (
+    <>
+      {headers.map((header, i) => (
+        <span className="col-span-3 pt-2 size-full text-center text-accent-content">
+          {header}
+        </span>
+      ))}
+    </>
+  );
+};
+
+const Input = () => {
   return (
     <div className="col-span-12 grid grid-cols-12 items-center">
-      <div className="col-span-1">{icon}</div>
       <select
-        className="select select-ghost w-auto col-span-2"
+        className="select select-ghost w-auto col-span-3"
         defaultValue="WD"
       >
         <option disabled={true}>Amplifier</option>
@@ -42,7 +39,7 @@ const Input = ({
         <option>Amp</option>
       </select>
       <select
-        className="select select-ghost w-auto col-span-2"
+        className="select select-ghost w-auto col-span-3"
         defaultValue="WD"
       >
         <option disabled={true}>Attribute</option>
@@ -51,9 +48,16 @@ const Input = ({
         <option>HS</option>
       </select>
 
-      <label className="grow input input-ghost input-md w-full items-center col-span-7">
-        <input type="text" className="grow" placeholder={placeholder} />
-        <span>{field}</span>
+      <label className="grow input input-ghost input-md w-full items-center col-span-3">
+        <input type="text" className="grow" placeholder="Value" />
+      </label>
+      <label className="grow input input-ghost input-md w-full items-center col-span-3">
+        <input
+          type="text"
+          className="grow"
+          placeholder="Uptime"
+          defaultValue={1.0}
+        />
       </label>
     </div>
   );
@@ -63,36 +67,12 @@ export const Editor = () => {
   return (
     <div className="grid grid-cols-12 collapse-content px-3">
       <NameInput />
-      <Input
-        icon={<StopCircleIcon className="text-red-500 size-7" />}
-        field="Name"
-        placeholder="Item Name"
-      />
-      <Input
-        icon={<StopCircleIcon className="text-red-500 size-7" />}
-        field="Weapon Damage"
-        placeholder="Value"
-      />
-      <Input
-        icon={<StopCircleIcon className="text-red-500 size-7" />}
-        field="Critical Hit Chance"
-        placeholder="Value"
-      />
-      <Input
-        icon={<StopCircleIcon className="text-red-500 size-7" />}
-        field="Critical Hit Damage"
-        placeholder="Value"
-      />
-      <Input
-        icon={<Cog8ToothIcon className="text-red-500 size-7" />}
-        field="Headshot Damage"
-        placeholder="Value"
-      />
-      <Input
-        icon={<CubeIcon className="text-base-content size-7" />}
-        field="Talents"
-        placeholder="Value"
-      />
+      <Header />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </div>
   );
 };
