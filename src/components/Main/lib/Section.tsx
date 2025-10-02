0b0e64e36ae92a201f8005126087459d7835730c
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-02 08:18:37 +0800

    center align card title

--- src/components/Main/lib/Section.tsx
+++ src/components/Main/lib/Section.tsx
@@ -13,7 +13,7 @@ export const Section = ({
   return (
     <div className="collapse collapse-arrow bg-base-100 border-base-300 border">
       {control}
-      <div className="collapse-title font-semibold">{name}</div>
+      <div className="collapse-title p-3 text-center font-semibold">{name}</div>
       <div className="collapse-content text-sm">{children}</div>
     </div>
   );
