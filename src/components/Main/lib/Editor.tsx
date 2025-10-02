8dc7465155b55ff526dc27ecf3b4f36a948cc8d1
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-02 11:00:20 +0800

    very basic editor example

--- src/components/Main/lib/Editor.tsx
+++ src/components/Main/lib/Editor.tsx
@@ -1,7 +1,61 @@
+import { ReactNode } from "react";
+import {
+  StopCircleIcon,
+  Cog8ToothIcon,
+  CubeIcon,
+} from "@heroicons/react/24/solid";
+
+const Input = ({
+  icon,
+  field,
+  placeholder,
+}: {
+  icon: ReactNode;
+  field: string;
+  placeholder: string;
+}) => {
+  return (
+    <label className="input input-ghost input-md w-full items-center">
+      {icon}
+      <span>{field}</span>
+      <input type="text" className="grow" placeholder={placeholder} />
+    </label>
+  );
+};
+
 export const Editor = () => {
   return (
     <div className="collapse-content px-3">
-      <span>Awesome attribute editor here.</span>
+      <Input
+        icon={<StopCircleIcon className="text-red-500 size-7" />}
+        field="Name"
+        placeholder="Item Name"
+      />
+      <Input
+        icon={<StopCircleIcon className="text-red-500 size-7" />}
+        field="Weapon Damage"
+        placeholder="Value"
+      />
+      <Input
+        icon={<StopCircleIcon className="text-red-500 size-7" />}
+        field="Critical Hit Chance"
+        placeholder="Value"
+      />
+      <Input
+        icon={<StopCircleIcon className="text-red-500 size-7" />}
+        field="Critical Hit Damage"
+        placeholder="Value"
+      />
+      <Input
+        icon={<Cog8ToothIcon className="text-red-500 size-7" />}
+        field="Headshot Damage"
+        placeholder="Value"
+      />
+      <Input
+        icon={<CubeIcon className="text-base-content size-7" />}
+        field="Talents"
+        placeholder="Value"
+      />
     </div>
   );
 };
