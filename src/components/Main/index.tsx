f4e0951ed1b6240b4136f0f34a0f152de4cc29b6
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-02 23:56:09 +0800

    Extras Section, added Specialization to Extras

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,13 @@
 import Weapons from "./Weapons";
 import Gears from "./Gears";
-import Watch from "./Watch";
+import Extras from "./Extras";
 
 export default function Main() {
   return (
     <div className="flex-grow overflow-auto">
       <Weapons />
       <Gears />
-      <Watch />
+      <Extras />
       <div className="w-full p-2 flex flex-row justify-center">
         <button className="btn btn-primary btn-outline">Save to Loadout</button>
       </div>
