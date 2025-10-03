eaa48c89db676dfee9fa0576dffee4c5d8ae4e58
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-03 18:01:44 +0800

    simplify Main store

--- src/store/ui/index.ts
+++ src/store/ui/index.ts
@@ -1,5 +1,5 @@
 import { store as NavBar } from "@/store/ui/NavBar";
-import { stores as Main } from "@/store/ui/Main";
+import { store as Main } from "@/store/ui/Main";
 import { store as Dock } from "@/store/ui/Dock";
 
 export const stores = {
