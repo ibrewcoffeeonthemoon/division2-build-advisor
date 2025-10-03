1ed59c935e85377207f1ede18b4696aa6720dd37
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-03 08:03:42 +0800

    cleanup

--- src/components/Main/lib/Attribute.tsx
+++ src/components/Main/lib/Attribute.tsx
@@ -1,24 +1,6 @@
-import { ReactNode } from "react";
-
 export type Attribute = {
   type: "Attribute" | "Mod" | "Talent";
   name: string;
   value: number;
   uptime: number;
-};
-
-type Props = {
-  icon: ReactNode;
-  field: string;
-  val: string;
-};
-
-export const ItemAttribute = ({ icon, field, val }: Props) => {
-  return (
-    <tr className="">
-      <td className="w-5 h-5">{icon}</td>
-      <td className="pl-2">{val}</td>
-      <td className="pl-2">{field}</td>
-    </tr>
-  );
 };
