7e193c6d395cbad71054ba8f7c0786fc3f3adf53
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-03 11:56:47 +0800

    define all valid types under Categories namespace

--- src/lib/type/categories.ts
+++ src/lib/type/categories.ts
@@ -1,1 +1,9 @@
 export type Weapons = "Primary" | "Secondary" | "Sidearm" | "Signature";
+export type Gears =
+  | "Mask"
+  | "Backpack"
+  | "Chest"
+  | "Gloves"
+  | "Holster"
+  | "Kneepads";
+export type Extras = "Watch" | "Specialization";
