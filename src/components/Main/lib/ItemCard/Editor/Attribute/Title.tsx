d04a125bacc99f9f5257cb8943c07f856d8e2e41
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-04 06:34:18 +0800

    number format

--- src/components/Main/lib/ItemCard/Editor/Attribute/Title.tsx
+++ src/components/Main/lib/ItemCard/Editor/Attribute/Title.tsx
@@ -4,8 +4,12 @@ export const Title = ({ attribute }: { attribute: Attribute }) => {
   return (
     <>
       <h2 className="col-span-7 w-full text-center">{attribute.name}</h2>
-      <h2 className="col-span-3 w-full text-center">{attribute.value}</h2>
-      <h2 className="col-span-2 w-full text-center">{attribute.uptime}</h2>
+      <h2 className="col-span-3 w-full text-center">
+        {(attribute.value * 100).toFixed(1)}
+      </h2>
+      <h2 className="col-span-2 w-full text-center">
+        {(attribute.uptime * 100).toFixed(0)}
+      </h2>
     </>
   );
 };
