a4947ea18e3bc27f2e684fa64f3857d60f409b8f
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-04 07:44:48 +0800

    optional openedIndex in zustand store

--- src/store/ui/Main.ts
+++ src/store/ui/Main.ts
@@ -4,12 +4,29 @@ import { persist } from "zustand/middleware";
 import { createSelectors } from "@/store/utils";
 import Sections from "@/lib/type/sections";
 
-type SectionState = { collapseOpen: boolean };
 type Store = {
   state: {
-    Weapons: SectionState;
-    Gears: SectionState;
-    Extras: SectionState;
+    Weapons: {
+      collapseOpen: boolean;
+      Primary: { openedIndex?: number };
+      Secondary: { openedIndex?: number };
+      Sidearm: { openedIndex?: number };
+      Signature: { openedIndex?: number };
+    };
+    Gears: {
+      collapseOpen: boolean;
+      Mask: { openedIndex?: number };
+      Backpack: { openedIndex?: number };
+      Chest: { openedIndex?: number };
+      Gloves: { openedIndex?: number };
+      Holster: { openedIndex?: number };
+      Kneepads: { openedIndex?: number };
+    };
+    Extras: {
+      collapseOpen: boolean;
+      Watch: { openedIndex?: number };
+      Specialization: { openedIndex?: number };
+    };
   };
   setCollapseOpen: (section: Sections, val: boolean) => void;
 };
@@ -18,9 +35,27 @@ export const useStore = create<Store>()(
   persist(
     immer((set) => ({
       state: {
-        Weapons: { collapseOpen: false },
-        Gears: { collapseOpen: false },
-        Extras: { collapseOpen: false },
+        Weapons: {
+          collapseOpen: false,
+          Primary: {},
+          Secondary: {},
+          Sidearm: {},
+          Signature: {},
+        },
+        Gears: {
+          collapseOpen: false,
+          Mask: {},
+          Backpack: {},
+          Chest: {},
+          Gloves: {},
+          Holster: {},
+          Kneepads: {},
+        },
+        Extras: {
+          collapseOpen: false,
+          Watch: {},
+          Specialization: {},
+        },
       },
       setCollapseOpen: (section, val) =>
         set((s) => {
