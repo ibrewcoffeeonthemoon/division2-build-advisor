f23095561d34c4583cec850e1ebc6be8e46e677d
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-04 09:35:34 +0800

    stupid bug

--- src/store/ui/Main/action.ts
+++ src/store/ui/Main/action.ts
@@ -1,4 +1,8 @@
 export type Action = {
   setCollapseOpen: (section: string, val: boolean) => void;
-  setOpenedIndex: (section: string, category: string, val: null) => void;
+  setOpenedIndex: (
+    section: string,
+    category: string,
+    val: number | null,
+  ) => void;
 };
