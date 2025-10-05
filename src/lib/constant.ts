5506d083f9d6ed47f5eef1a01b03527072e0d54b
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-05 09:36:38 +0800

    define AMPLIFIERS and Amplifier

---
+++ src/lib/constant.ts
@@ -0,0 +1,10 @@
+export const AMPLIFIERS = [
+  "WD",
+  "TWD",
+  "AMP1",
+  "AMP2",
+  "AMP3",
+  "CRIT_HS",
+  "DTA_DTH",
+  "DTTOOC",
+] as const;
--- src/lib/type/index.ts
+++ src/lib/type/index.ts
@@ -1,4 +1,8 @@
+import { AMPLIFIERS } from "../constant";
+
 export type AttributeType = "Attribute" | "Mod" | "Talent";
+
+export type Amplifier = (typeof AMPLIFIERS)[number];
 
 export type Attribute = {
   type: AttributeType;
@@ -6,6 +10,7 @@ export type Attribute = {
   value: number;
   uptime: number;
   note: string;
+  amplifier: Amplifier;
 };
 
 export const DEFAULT_ATTRIBUTE: Attribute = {
@@ -14,4 +19,5 @@ export const DEFAULT_ATTRIBUTE: Attribute = {
   value: 0.15,
   uptime: 1.0,
   note: "",
+  amplifier: "WD",
 };
