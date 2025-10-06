88970d82b1c58c15a406c1e0f5a6e8ad13caa1fc
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-06 13:14:25 +0800

    return Damage as package

--- src/lib/damage.ts
+++ src/lib/damage.ts
@@ -1,12 +1,17 @@
 import { State } from "@/store/data/state";
 
-export const damage = (s: State["state"]) => {
+export type Damage = {
+  dmg: number | null;
+  dps: number | null;
+};
+
+export const damage = (s: State["state"]): Damage => {
   const primary = s["Weapons"]["Primary"];
   const baseDamage = primary.baseDamage;
   const rpm = primary.rpm;
-  if (baseDamage !== null && rpm !== null) {
-    const dps = baseDamage * rpm;
-    return dps;
-  }
-  return null;
+  const valid = baseDamage !== null && rpm !== null;
+  return {
+    dmg: baseDamage,
+    dps: valid ? (baseDamage * rpm) / 60 : null,
+  };
 };
