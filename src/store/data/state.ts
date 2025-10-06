43a525b0969d79470b9900b23902dff4bb9036b7
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-06 10:31:21 +0800

    apply only to Weapons section

--- src/store/data/state.ts
+++ src/store/data/state.ts
@@ -13,7 +13,12 @@ export type State = {
 };
 
 export const state: () => State["state"] = () =>
-  createItemRecords(() => ({
-    name: "",
-    attributes: [],
-  }));
+  createItemRecords((section) => {
+    const inWeapons = section === "Weapons";
+    return {
+      name: "",
+      baseDamage: inWeapons ? 48_500 : null,
+      rpm: inWeapons ? 850 : null,
+      attributes: [],
+    };
+  });
