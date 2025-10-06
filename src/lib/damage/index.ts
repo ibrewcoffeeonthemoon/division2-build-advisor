43ea7eb846f64082cd17123df6e70df1f855c020
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-07 00:33:25 +0800

    basic complete damage formula

--- src/lib/damage/index.ts
+++ src/lib/damage/index.ts
@@ -19,9 +19,29 @@ const calDmg = (item: Items<"Weapons">, s: State["state"]): Dmg => {
   const weapon = s["Weapons"][item];
   const baseDamage = weapon.baseDamage!;
   const ampSums = calAmplifierSums(s);
-  const mulWDCore = 1 + (ampSums[item].WDCore || 0);
+  const m = ampSums[item];
+
+  // prettier-ignore
+  const multiplier = 
+    // 1 + Weapon Damage + Weapon Type Damage + Weapon Damage Talent
+    (1 + (m.WDCore || 0) + (m.WDType || 0) + (m.WDTalent || 0)) *
+    // 1 + Total Weapon Damage
+    (1 + ( m.TWD || 0 )) *
+    // 1 + Amplifier1
+    (1 + ( m.AMP1 || 0 )) *
+    // 1 + Amplifier2
+    (1 + ( m.AMP2 || 0 )) *
+    // 1 + Amplifier3
+    (1 + ( m.AMP3 || 0 )) *
+    // 1 + Critical Hit Chance * Critical Hit Damage + Headshot Damage
+    (1 + ( m.CHC || 0 ) * ( m.CHD || 0 ) + ( m.HS || 0 )) *
+    // 1 + Damage to Armor + Damage to Health
+    (1 + ( m.DTA_DTH || 0 )) *
+    // 1 + Damage to Target out of Cover
+    (1 + ( m.DTTOOC || 0 ));
+
   return {
-    normal: baseDamage * mulWDCore,
+    normal: baseDamage * multiplier,
   };
 };
 
