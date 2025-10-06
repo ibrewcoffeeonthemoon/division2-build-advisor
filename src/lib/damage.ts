d66dbc4acfb88ddeef21fff660e0d48fac707bc1
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-06 19:26:31 +0800

    calc chain

--- src/lib/damage.ts
+++ src/lib/damage.ts
@@ -1,18 +1,44 @@
 import { State } from "@/store/data/state";
 import { Items } from "./type";
 
+type Dmg = {
+  normal: number | null;
+  critical: number | null;
+  critical_headshot: number | null;
+};
+
+const calDmg = (item: Items<"Weapons">, s: State["state"]): Dmg => {
+  const weapon = s["Weapons"][item];
+  const dmg = weapon.baseDamage;
+  return {
+    normal: dmg,
+    critical: dmg,
+    critical_headshot: dmg,
+  };
+};
+
+type Dps = {
+  dmg: Dmg;
+  dps: number | null;
+};
+
+const calDps = (item: Items<"Weapons">, s: State["state"]): Dps => {
+  const weapon = s["Weapons"][item];
+  const dmg = calDmg(item, s);
+  const rpm = weapon.rpm;
+  const dps = (dmg.normal * rpm) / 60;
+  return { dmg, dps };
+};
+
 export type Damage = {
   dmg: number | null;
   dps: number | null;
 };
 
 export const damage = (item: Items<"Weapons">, s: State["state"]): Damage => {
-  const primary = s["Weapons"][item];
-  const baseDamage = primary.baseDamage;
-  const rpm = primary.rpm;
-  const valid = baseDamage !== null && rpm !== null;
+  const result = calDps(item, s);
   return {
-    dmg: baseDamage,
-    dps: valid ? (baseDamage * rpm) / 60 : null,
+    dmg: result.dmg.normal,
+    dps: result.dps,
   };
 };
