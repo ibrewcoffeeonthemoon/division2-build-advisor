ca560e7d2484a5149036ee3f97119a9f89bab5d8
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-07 14:53:27 +0800

    apply to multiplier

--- src/lib/damage/multiplier.ts
+++ src/lib/damage/multiplier.ts
@@ -1,13 +1,8 @@
 import { Items } from "../type";
 import { AmplifierSums } from "./amplifier";
+import { createDamageRecord, DamageRecord } from "./record";
 
-// prettier-ignore
-export type Multiplier = 
-  Record<"normal" | "critical", 
-    Record<"headshot" | "bodyshot", 
-      Record<"health" | "armor", 
-        Record<"cover"| "nocover", number>>>
->;
+type Multiplier = DamageRecord<number>;
 
 export const calMultiplier = (
   amplifierSums: AmplifierSums[Items<"Weapons">],
@@ -26,33 +21,18 @@ export const calMultiplier = (
     // 1 + Amplifier3
     (1 + (m.AMP3 || 0));
 
-  const result: Record<
-    string,
-    Record<string, Record<string, Record<string, number>>>
-  > = {};
-
-  ["normal", "critical"].forEach((n0) => {
-    result[n0] = {};
-    ["headshot", "bodyshot"].forEach((n1) => {
-      result[n0][n1] = {};
-      ["armor", "health"].forEach((n2) => {
-        result[n0][n1][n2] = {};
-        ["cover", "nocover"].forEach((n3) => {
-          result[n0][n1][n2][n3] = x;
-          //
-          let y = 1;
-          // 1 + Critical Hit Chanmce * Critical Hit Damage + Headshot Damage.
-          y += n0 === "critical" ? (m.CHC || 0) + (m.CHD || 0) : 0;
-          y += n1 === "headshot" ? m.HS || 0 : 0;
-          // 1 + Damage to Armor + Damage to Health.
-          y *= n2 === "armor" ? 1 + (m.DTA || 0) : 1 + (m.DTH || 0);
-          // 1 + Damage to Target out of Cover
-          y *= n3 === "cover" ? 1 : 1 + (m.DTTOOC || 0);
-          //
-          result[n0][n1][n2][n3] *= y;
-        });
-      });
-    });
+  const result = createDamageRecord((n0, n1, n2, n3) => {
+    //
+    let y = 1;
+    // 1 + Critical Hit Chanmce * Critical Hit Damage + Headshot Damage.
+    y += n0 === "critical" ? (m.CHC || 0) + (m.CHD || 0) : 0;
+    y += n1 === "headshot" ? m.HS || 0 : 0;
+    // 1 + Damage to Armor + Damage to Health.
+    y *= n2 === "armor" ? 1 + (m.DTA || 0) : 1 + (m.DTH || 0);
+    // 1 + Damage to Target out of Cover
+    y *= n3 === "cover" ? 1 : 1 + (m.DTTOOC || 0);
+    //
+    return x * y;
   });
 
   return result as Multiplier;
