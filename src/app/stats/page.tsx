b46650c6473b0561c7d67088b0566438ca0e7df7
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-07 12:37:05 +0800

    stats page

---
+++ src/app/stats/page.tsx
@@ -0,0 +1,26 @@
+import Dock from "@/components/Dock";
+import NavBar from "@/components/NavBar";
+
+export default function Stats() {
+  return (
+    <div
+      className="
+        h-full w-full fixed self-auto
+        grid-cols-1 place-items-center
+      "
+    >
+      <div
+        className="
+          h-full w-full md:max-w-xl
+          flex flex-col
+        "
+      >
+        <NavBar />
+        <div className="flex-grow overflow-auto flex flex-col justify-center items-center">
+          <h1 className="text-5xl">Stats</h1>
+        </div>
+        <Dock />
+      </div>
+    </div>
+  );
+}
