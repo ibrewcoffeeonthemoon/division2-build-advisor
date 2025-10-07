1aaec03174d7e5c2c58a5d0721783348838723db
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-07 18:26:22 +0800

    Dock include all href links

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
+import Link from "next/link";
 import {
   ArchiveBoxIcon,
   WrenchScrewdriverIcon,
@@ -12,35 +13,38 @@ const Button = ({
   id,
   name,
   icon,
+  href,
 }: {
   id: number;
   name: string;
   icon: ReactNode;
+  href: string;
 }) => {
   const activeButton = store.activeButton();
   const setActiveButton = store.setActiveButton();
 
   return (
-    <button
+    <Link
       className={`${activeButton === id ? "dock-active" : ""}`}
       onClick={() => setActiveButton(id)}
+      href={href}
     >
       <span className="w-6 h-6">{icon}</span>
       <span className="dock-label">{name}</span>
-    </button>
+    </Link>
   );
 };
 
 export default function Dock() {
   const buttons = [
-    { name: "Loadout", icon: <ArchiveBoxIcon /> },
-    { name: "Build", icon: <WrenchScrewdriverIcon /> },
-    { name: "Stats", icon: <ChartBarIcon /> },
+    { name: "Loadout", href: "/loadout", icon: <ArchiveBoxIcon /> },
+    { name: "Build", href: "/build", icon: <WrenchScrewdriverIcon /> },
+    { name: "Stats", href: "/stats", icon: <ChartBarIcon /> },
   ];
   return (
     <div className="dock static bg-base-200 border-y-2 border-base-300">
-      {buttons.map(({ name, icon }, id) => (
-        <Button key={id} {...{ name, icon, id }} />
+      {buttons.map(({ name, icon, href }, id) => (
+        <Button key={id} {...{ name, icon, id, href }} />
       ))}
     </div>
   );
