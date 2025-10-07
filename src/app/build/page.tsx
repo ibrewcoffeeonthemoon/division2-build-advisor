a83e6fc01a6fdee6d10d4b85a405bfe1f3b27430
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-07 18:20:47 +0800

    build page

---
+++ src/app/build/page.tsx
@@ -0,0 +1,25 @@
+import Dock from "@/components/Dock";
+import Main from "@/components/Main";
+import NavBar from "@/components/NavBar";
+
+export default function Build() {
+  return (
+    <div
+      className="
+        h-full w-full fixed self-auto
+        grid-cols-1 place-items-center
+      "
+    >
+      <div
+        className="
+          h-full w-full md:max-w-xl
+          flex flex-col
+        "
+      >
+        <NavBar />
+        <Main />
+        <Dock />
+      </div>
+    </div>
+  );
+}
