c8a3dbcc3d1fadfbb33118d0f73a0b145d041a68
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-07 19:50:47 +0800

    Stats dir

---
+++ src/components/Stats/index.tsx
@@ -0,0 +1,7 @@
+export default function Stats() {
+  return (
+    <div className="flex-grow overflow-auto flex flex-col justify-center items-center">
+      <h1 className="text-5xl">Stats</h1>
+    </div>
+  );
+}
