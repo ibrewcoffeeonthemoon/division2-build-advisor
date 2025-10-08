e0e72682a2a55695be7ed8b18fed849608bd4550
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-08 14:09:06 +0800

    setBuild

--- src/store/data/action.ts
+++ src/store/data/action.ts
@@ -1,6 +1,13 @@
-import { Amplifier, Attribute, AttributeType, WeaponType } from "@/lib/type";
+import {
+  Amplifier,
+  Attribute,
+  AttributeType,
+  Build,
+  WeaponType,
+} from "@/lib/type";
 
 export type Action = {
+  setBuild: (val: Build) => void;
   setBuildName: (val: string) => void;
   setName: (sec: string, item: string, val: string) => void;
   setBaseDamage: (sec: string, item: string, val: number | null) => void;
