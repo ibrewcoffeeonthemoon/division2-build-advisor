9cee7cdbe3a0ac05b9dd9cbf1a48e2ac4587e80e
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-08 12:13:10 +0800

    display all weapons in a grid

--- src/components/Loadout/BuildCard.tsx
+++ src/components/Loadout/BuildCard.tsx
@@ -1,4 +1,5 @@
 import { Build } from "@/lib/type";
+import { Fragment } from "react";
 
 export default function BuildCard({
   key,
@@ -11,9 +12,23 @@ export default function BuildCard({
     <div key={key} className="card">
       <div className="card-body">
         <div className="card-title">{build.name}</div>
-        <div className="">Attribute</div>
-        <div className="">Attribute</div>
-        <div className="">Attribute</div>
+        <div className="grid grid-cols-24">
+          <span className="col-span-10 col-start-6 font-semibold">Name</span>
+          <span className="col-span-6 font-semibold">DMG</span>
+          <span className="col-span-3 font-semibold">RPM</span>
+          {Object.entries(build.items.Weapons).map(([key, item], i) => (
+            <Fragment key={i}>
+              <span className="col-span-5 col-start-1 font-semibold">
+                {key}
+              </span>
+              <span className="col-span-10 font-semibold text-primary">
+                {item.name}
+              </span>
+              <span className="col-span-6">{"48,500"}</span>
+              <span className="col-span-3">{"850"}</span>
+            </Fragment>
+          ))}
+        </div>
       </div>
     </div>
   );
