cf3790f13e7b197489780f4c45c1e2050d290d01
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-08 11:03:30 +0800

    demo on how to load record to loadout

--- src/components/Loadout/index.tsx
+++ src/components/Loadout/index.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { store } from "@/store/loadout";
+
 export default function Loadout() {
+  const builds = store.state().builds;
+
   return (
     <div className="flex-grow overflow-auto flex flex-col justify-center items-center">
       <h1 className="text-5xl">Loadout</h1>
+      {builds.map((build, i) => (
+        <div key={i}>{build.name}</div>
+      ))}
     </div>
   );
 }
