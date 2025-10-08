70d5eb6bd4000b23b639e53a9ab7e1c0bfbd087e
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-08 19:50:41 +0800

    setTopicOpen

--- src/components/Stats/lib/ItemCard.tsx
+++ src/components/Stats/lib/ItemCard.tsx
@@ -13,7 +13,7 @@ export const ItemCard = <C extends string, M extends string>({
   children,
 }: ItemCardProps<C, M>) => {
   const open = store.state().section.topic.open[category][item];
-  const setOpen = store.setItemOpen();
+  const setOpen = store.setTopicOpen();
 
   return (
     <div className="collapse collapse-arrow rounded-md border-1 border-base-300 duration-1000">
