eac9613eb8ace7993b0f3406cfa9121bc81dcd6b
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-08 09:37:56 +0800

    factor out style Cell

--- src/components/Stats/Damage/Spreadsheet.tsx
+++ src/components/Stats/Damage/Spreadsheet.tsx
@@ -3,38 +3,72 @@ import { Items } from "@/lib/type";
 import { round } from "@/lib/utils";
 import { stores } from "@/store";
 
+const Cell = ({ style, text }: { style?: string; text: string }) => (
+  <span className={`col-span-3 ${style}`}>{text}</span>
+);
+
 export const Spreadsheet = ({ weapon }: { weapon: Items<"Weapons"> }) => {
   const { dmgRecord: d } = calDamage(weapon, stores.data.state());
   const format = (x: number) => round(x, 0).toLocaleString();
 
   return (
     <>
-      <span className="col-span-3">health inC</span>
-      <span className="col-span-3">
-        {format(d.normal.bodyshot.health.cover)}
-      </span>
-      <span className="col-span-3 text-red-700">
-        {format(d.normal.headshot.health.cover)}
-      </span>
-      <span className="col-span-3 text-orange-400">
-        {format(d.critical.bodyshot.health.cover)}
-      </span>
-      <span className="col-span-3 text-orange-400 font-bold">
-        {format(d.critical.headshot.health.cover)}
-      </span>
-      <span className="col-span-3">health ooC</span>
-      <span className="col-span-3">
-        {format(d.normal.bodyshot.health.nocover)}
-      </span>
-      <span className="col-span-3 text-red-700">
-        {format(d.normal.headshot.health.nocover)}
-      </span>
-      <span className="col-span-3 text-orange-400">
-        {format(d.critical.bodyshot.health.nocover)}
-      </span>
-      <span className="col-span-3 text-orange-400 font-bold">
-        {format(d.critical.headshot.health.nocover)}
-      </span>
+      <Cell text="Health inC" />
+      <Cell text={format(d.normal.bodyshot.health.cover)} />
+      <Cell
+        style="text-red-700"
+        text={format(d.normal.headshot.health.cover)}
+      />
+      <Cell
+        style="text-orange-400"
+        text={format(d.critical.bodyshot.health.cover)}
+      />
+      <Cell
+        style="text-orange-400 font-bold"
+        text={format(d.critical.headshot.health.cover)}
+      />
+
+      <Cell text="Health ooC" />
+      <Cell text={format(d.normal.bodyshot.health.nocover)} />
+      <Cell
+        style="text-red-700"
+        text={format(d.normal.headshot.health.nocover)}
+      />
+      <Cell
+        style="text-orange-400"
+        text={format(d.critical.bodyshot.health.nocover)}
+      />
+      <Cell
+        style="text-orange-400 font-bold"
+        text={format(d.critical.headshot.health.nocover)}
+      />
+
+      <Cell text="Armor inC" />
+      <Cell text={format(d.normal.bodyshot.armor.cover)} />
+      <Cell style="text-red-700" text={format(d.normal.headshot.armor.cover)} />
+      <Cell
+        style="text-orange-400"
+        text={format(d.critical.bodyshot.armor.cover)}
+      />
+      <Cell
+        style="text-orange-400 font-bold"
+        text={format(d.critical.headshot.armor.cover)}
+      />
+
+      <Cell text="Armor ooC" />
+      <Cell text={format(d.normal.bodyshot.armor.nocover)} />
+      <Cell
+        style="text-red-700"
+        text={format(d.normal.headshot.armor.nocover)}
+      />
+      <Cell
+        style="text-orange-400"
+        text={format(d.critical.bodyshot.armor.nocover)}
+      />
+      <Cell
+        style="text-orange-400 font-bold"
+        text={format(d.critical.headshot.armor.nocover)}
+      />
     </>
   );
 };
