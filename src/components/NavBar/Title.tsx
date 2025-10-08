5ca3930eba732a8df25e9eaf1cc85402705747e6
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-08 13:27:51 +0800

    dynamic title

--- src/components/NavBar/Title.tsx
+++ src/components/NavBar/Title.tsx
@@ -3,12 +3,12 @@ import { stores } from "@/store";
 export const Title = () => {
   const activeButton = stores.ui.Dock.activeButton();
   const buildName = stores.data.state().name;
+  const titles = ["Loadout", buildName, "Stats"];
+  const title = activeButton === null ? "" : titles[activeButton];
 
   return (
     <div className="flex-1 justify-center items-center">
-      <h1 className="font-xl font-bold text-center align-middle">
-        {activeButton === 1 ? buildName : "Build Advisor"}
-      </h1>
+      <h1 className="font-xl font-bold text-center align-middle">{title}</h1>
     </div>
   );
 };
