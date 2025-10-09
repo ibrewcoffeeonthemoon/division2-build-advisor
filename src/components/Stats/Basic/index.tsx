5ea0b0210e754ea0800ef60fe000854c61599575
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-09 18:44:04 +0800

    a basic working basic stats

--- src/components/Stats/Basic/index.tsx
+++ src/components/Stats/Basic/index.tsx
@@ -4,6 +4,8 @@ import { stores } from "@/store";
 import { SCHEMA } from "@/lib/constant";
 import { ItemCard } from "../lib/ItemCard";
 import { Section } from "@/components/lib/Section";
+import { calAmplifierSums } from "@/lib/damage/amplifier";
+import { Items } from "@/lib/type";
 
 export default function Basic() {
   const section = "Basic";
@@ -11,6 +13,7 @@ export default function Basic() {
   const setOpen = stores.ui.Stats.action().setSectionOpen;
   const baseDamageReady = (item: string) =>
     (stores.edit.state().items["Weapons"][item].baseDamage ?? 0) > 0;
+  const amplifierSums = calAmplifierSums(stores.edit.state());
 
   return (
     <Section
@@ -28,7 +31,22 @@ export default function Basic() {
           baseDamageReady(item) && (
             <ItemCard key={i} category={section} item={item}>
               <div className="col-span-12 grid grid-cols-15 p-0 text-right">
-                CHC CHD HS
+                <span className="col-span-9 text-bold">
+                  Critical Hit Chance
+                </span>
+                <span className="col-span-6 text-info">
+                  {amplifierSums[item as Items<"Weapons">].CHC * 100}%
+                </span>
+                <span className="col-span-9 text-bold">
+                  Critical Hit Damage
+                </span>
+                <span className="col-span-6 text-info">
+                  {amplifierSums[item as Items<"Weapons">].CHD * 100}%
+                </span>
+                <span className="col-span-9 text-bold">Headshot Damage</span>
+                <span className="col-span-6 text-info">
+                  {amplifierSums[item as Items<"Weapons">].HS * 100}%
+                </span>
               </div>
             </ItemCard>
           ),
