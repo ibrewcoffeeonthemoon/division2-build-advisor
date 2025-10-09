57773631f18f3eb0dde2fbfb52ea8cf9f3daf7f5
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-09 12:18:40 +0800

    allow type arg

--- src/lib/presets/helper.ts
+++ src/lib/presets/helper.ts
@@ -1,33 +1,49 @@
-import { Attribute } from "../type/attribute";
+import { Attribute, AttributeType } from "../type/attribute";
 
-export const WD = (value?: number, uptime?: number): Attribute => ({
+export const WD = (
+  value?: number,
+  uptime?: number,
+  type?: AttributeType,
+): Attribute => ({
   name: "Weapon Damage",
   amplifier: "WDCore",
-  type: "Attribute",
+  type: type ?? "Attribute",
   uptime: uptime ?? 1,
   value: value ?? 0.15,
 });
 
-export const CHC = (value?: number, uptime?: number): Attribute => ({
+export const CHC = (
+  value?: number,
+  uptime?: number,
+  type?: AttributeType,
+): Attribute => ({
   name: "Critical Hit Chance",
   amplifier: "CHC",
-  type: "Attribute",
+  type: type ?? "Attribute",
   uptime: uptime ?? 1,
   value: value ?? 0.06,
 });
 
-export const CHD = (value?: number, uptime?: number): Attribute => ({
+export const CHD = (
+  value?: number,
+  uptime?: number,
+  type?: AttributeType,
+): Attribute => ({
   name: "Critical Hit Damage",
   amplifier: "CHD",
-  type: "Attribute",
+  type: type ?? "Attribute",
   uptime: uptime ?? 1,
   value: value ?? 0.12,
 });
 
-export const HS = (value?: number, uptime?: number): Attribute => ({
+export const HS = (
+  value?: number,
+  uptime?: number,
+  type?: AttributeType,
+): Attribute => ({
   name: "Critical Hit Damage",
   amplifier: "CHD",
-  type: "Attribute",
+  type: type ?? "Attribute",
   uptime: uptime ?? 1,
   value: value ?? 0.1,
 });
