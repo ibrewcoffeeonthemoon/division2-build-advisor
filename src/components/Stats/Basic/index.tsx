c4bad531a27569a61357995751aba68f76f73589
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-09 18:32:39 +0800

    Basic Stats page

---
+++ src/components/Stats/Basic/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { stores } from "@/store";
+import { SCHEMA } from "@/lib/constant";
+import { ItemCard } from "../lib/ItemCard";
+import { Section } from "@/components/lib/Section";
+
+export default function Basic() {
+  const section = "Basic";
+  const open = stores.ui.Stats.state().section.open[section];
+  const setOpen = stores.ui.Stats.action().setSectionOpen;
+
+  return (
+    <Section
+      name={section}
+      control={
+        <input
+          type="checkbox"
+          checked={open}
+          onChange={(e) => setOpen(section, e.currentTarget.checked)}
+        />
+      }
+    >
+      {Object.keys(SCHEMA.Weapons).map((item, i) => (
+        <ItemCard key={i} category={section} item={item}>
+          <div className="col-span-12 grid grid-cols-15 p-0 text-right">
+            CHC CHD HS
+          </div>
+        </ItemCard>
+      ))}
+    </Section>
+  );
+}
--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -1,8 +1,10 @@
+import Basic from "./Basic";
 import Damage from "./Damage";
 
 export default function Stats() {
   return (
     <div className="flex-grow overflow-auto">
+      <Basic />
       <Damage />
     </div>
   );
