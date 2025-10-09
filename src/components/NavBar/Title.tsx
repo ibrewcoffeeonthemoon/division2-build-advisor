359259699745f0306cc9193601eb9e104b645abc
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-09 14:15:11 +0800

    Stats also show build name

--- src/components/NavBar/Title.tsx
+++ src/components/NavBar/Title.tsx
@@ -3,7 +3,7 @@ import { stores } from "@/store";
 export const Title = () => {
   const activeButton = stores.ui.Dock.state().activeButton;
   const buildName = stores.edit.state().name;
-  const titles = ["Loadout", buildName, "Stats"];
+  const titles = ["Loadout", buildName, buildName];
   const title = activeButton === null ? "" : titles[activeButton];
 
   return (
