79cfe60b3c3d39fa17102105df21f9cd23c6571b
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-10 23:45:25 +0800

    stronger type of the STATE

--- src/store/ui/Stats/state.ts
+++ src/store/ui/Stats/state.ts
@@ -9,16 +9,27 @@ export const TOPIC_NAMES = [
 export type SectionName = (typeof SECTION_NAMES)[number];
 export type TopicName = (typeof TOPIC_NAMES)[number];
 
+type SectionRecord = {
+  [S in SectionName]: boolean;
+};
+type TopicRecord = {
+  [S in SectionName]: {
+    [T in TopicName]: boolean;
+  };
+};
+
 export const STATE = {
   section: {
-    open: Object.fromEntries(SECTION_NAMES.map((sec) => [sec, false])),
+    open: Object.fromEntries(
+      SECTION_NAMES.map((sec) => [sec, false]),
+    ) as SectionRecord,
     topic: {
       open: Object.fromEntries(
         SECTION_NAMES.map((sec) => [
           sec,
           Object.fromEntries(TOPIC_NAMES.map((top) => [top, false])),
         ]),
-      ),
+      ) as TopicRecord,
     },
   },
 } as const;
