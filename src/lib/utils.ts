629de00e7bad53c2bb37da26252603d99d0ed825
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-10 23:58:25 +0800

    a type safe fromEntriesOf function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,11 @@ export const round = (x: number, n: number) => {
 export const keysOf = <T extends object>(obj: T): Array<keyof T> => {
   return Object.keys(obj) as Array<keyof T>;
 };
+
+export const fromEntriesOf = <
+  const T extends ReadonlyArray<readonly [PropertyKey, unknown]>,
+>(
+  entries: T,
+): { [K in T[number] as K[0]]: K[1] } => {
+  return Object.fromEntries(entries) as { [K in T[number] as K[0]]: K[1] };
+};
--- src/store/ui/Stats/state.ts
+++ src/store/ui/Stats/state.ts
@@ -1,3 +1,5 @@
+import { fromEntriesOf } from "@/lib/utils";
+
 export const SECTION_NAMES = ["Basic", "Damage", "Dps"] as const;
 export const TOPIC_NAMES = [
   "Primary",
@@ -9,27 +11,16 @@ export const TOPIC_NAMES = [
 export type SectionName = (typeof SECTION_NAMES)[number];
 export type TopicName = (typeof TOPIC_NAMES)[number];
 
-type SectionRecord = {
-  [S in SectionName]: boolean;
-};
-type TopicRecord = {
-  [S in SectionName]: {
-    [T in TopicName]: boolean;
-  };
-};
-
 export const STATE = {
   section: {
-    open: Object.fromEntries(
-      SECTION_NAMES.map((sec) => [sec, false]),
-    ) as SectionRecord,
+    open: fromEntriesOf(SECTION_NAMES.map((sec) => [sec, false])),
     topic: {
-      open: Object.fromEntries(
+      open: fromEntriesOf(
         SECTION_NAMES.map((sec) => [
           sec,
-          Object.fromEntries(TOPIC_NAMES.map((top) => [top, false])),
+          fromEntriesOf(TOPIC_NAMES.map((top) => [top, false])),
         ]),
-      ) as TopicRecord,
+      ),
     },
   },
 } as const;
