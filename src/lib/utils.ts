4b307763b9361481db25a30a6d589ac65068a0e6
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-11 14:07:39 +0800

    better naming, fromEntries, simplified typing syntax

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,10 +7,8 @@ export const keys = <T extends object>(obj: T): (keyof T)[] => {
   return Object.keys(obj) as (keyof T)[];
 };
 
-export const fromEntriesOf = <
-  const T extends ReadonlyArray<readonly [PropertyKey, unknown]>,
->(
-  entries: T,
-): { [K in T[number] as K[0]]: K[1] } => {
-  return Object.fromEntries(entries) as { [K in T[number] as K[0]]: K[1] };
+export const fromEntries = <K extends string | number | symbol, V>(
+  entries: [K, V][],
+): Record<K, V> => {
+  return Object.fromEntries(entries) as Record<K, V>;
 };
