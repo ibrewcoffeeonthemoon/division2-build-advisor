b5f0f1ce253ac258c252407b7d341003ffd218c5
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-12 23:27:18 +0800

    update presets Striker

--- src/lib/presets/builds/LexingtonStriker.ts
+++ src/lib/presets/builds/LexingtonStriker.ts
@@ -7,14 +7,26 @@ export const LexingtonStriker: Build = {
   items: {
     Weapons: {
       Primary: {
+        name: "Bluescreen",
+        baseDamage: 62_361,
+        rpm: 720,
+        attributes: [
+          p.WDType({ wdtype: "LMG Damage" }),
+          p.DTTOOC({ value: 0.12 }),
+          p.DTA({ value: 0.06 }),
+          WeaponExpertise(30),
+          p.ROF({ value: 0.2 }),
+        ],
+      },
+      Secondary: {
         name: "Lexington",
         baseDamage: 48_700,
         rpm: 850,
         attributes: [
-          RedCore(),
           p.WDType({ wdtype: "AR Damage" }),
           p.DTH({ value: 0.21 }),
           p.DTTOOC({ value: 0.1 }),
+          WeaponExpertise(30),
           {
             amplifier: "AMP1",
             name: "< AMP1 >",
@@ -23,41 +35,38 @@ export const LexingtonStriker: Build = {
             value: 0.3,
             note: "Ranger",
           },
-          WeaponExpertise(30),
         ],
       },
-      Secondary: { name: "", attributes: [] },
       Sidearm: { name: "", attributes: [] },
       Signature: { name: "", attributes: [] },
     },
     Gears: {
       Mask: {
         name: "Coyote",
-        attributes: [RedCore(), p.CHC(), p.CHD(), p.CHD({ type: "Mod" })],
+        attributes: [RedCore(), p.CHC(), p.CHD(), p.CHC({ type: "Mod" })],
       },
       Backpack: {
         name: "Striker",
         attributes: [
           RedCore(),
           p.CHC(),
-          p.CHD(),
           p.CHD({ type: "Mod" }),
           {
             amplifier: "TWD",
             name: "< TWD >",
             type: "Talent",
             uptime: 1,
-            value: 0.65,
+            value: 0.9,
             note: "Risk Management",
           },
         ],
       },
       Chest: {
         name: "Lengmo",
-        attributes: [RedCore(), p.CHD(), p.CHD({ type: "Mod" })],
+        attributes: [p.CHD({ type: "Mod" })],
       },
-      Gloves: { name: "Striker", attributes: [RedCore(), p.CHD()] },
-      Holster: { name: "Striker", attributes: [RedCore(), p.CHD()] },
+      Gloves: { name: "Striker", attributes: [RedCore(), p.CHC()] },
+      Holster: { name: "Striker", attributes: [RedCore(), p.CHD(), p.ROF()] },
       Kneepads: { name: "Striker", attributes: [RedCore(), p.CHD()] },
     },
     Extras: {
