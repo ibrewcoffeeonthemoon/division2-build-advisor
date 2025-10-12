9e214e7ca50622d8a161871c18e2187c5434f3c0
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-12 18:26:56 +0800

    RowHeader

--- src/components/Stats/Damage/Multiplier/Table.tsx
+++ src/components/Stats/Damage/Multiplier/Table.tsx
@@ -1,6 +1,10 @@
 import { store } from "@/store/ui/Stats";
 import { SectionName, TopicName } from "@/store/ui/Stats/state";
 
+const RowHeader = ({ text }: { text: string }) => (
+  <span className="col-span-3 font-semibold">{text}</span>
+);
+
 export const Table = ({
   category,
   item,
@@ -17,38 +21,38 @@ export const Table = ({
   return (
     ready && (
       <div className="w-full grid grid-cols-12">
-        <span className="col-span-3">WD</span>
+        <RowHeader text="WD" />
         <span className="col-span-9">
           1{m.WDCore && `+ ${f(m.WDCore)}`}
           {m.WDType && `+ ${f(m.WDType)}`}
           {m.WDTalent && `+ ${f(m.WDTalent)}`}
         </span>
 
-        <span className="col-span-3">TWD</span>
+        <RowHeader text="TWD" />
         <span className="col-span-9">1{m.TWD && `+ ${f(m.TWD)}`}</span>
 
-        <span className="col-span-3">AMP1</span>
+        <RowHeader text="AMP1" />
         <span className="col-span-9">1{m.AMP1 && `+ ${f(m.AMP1)}`}</span>
 
-        <span className="col-span-3">AMP2</span>
+        <RowHeader text="AMP2" />
         <span className="col-span-9">1{m.AMP2 && `+ ${f(m.AMP2)}`}</span>
 
-        <span className="col-span-3">AMP3</span>
+        <RowHeader text="AMP3" />
         <span className="col-span-9">1{m.AMP3 && `+ ${f(m.AMP3)}`}</span>
 
-        <span className="col-span-3">CHD/HS</span>
+        <RowHeader text="CHD/HS" />
         <span className="col-span-9">
           1{n0 === "critical" && m.CHD && `+ ${f(m.CHD)}`}
           {n1 === "headshot" && m.HS && `+ ${f(m.HS)}`}
         </span>
 
-        <span className="col-span-3">DTA/DTH</span>
+        <RowHeader text="DTA/DTH" />
         <span className="col-span-9">
           1{n2 === "health" && m.DTH && `+ ${f(m.DTH)}`}
           {n2 === "armor" && m.DTA && `+ ${f(m.DTA)}`}
         </span>
 
-        <span className="col-span-3">DTTOOC</span>
+        <RowHeader text="DTTOOC" />
         <span className="col-span-9">
           1{n3 === "nocover" && m.DTTOOC && `+ ${f(m.DTTOOC)}`}
         </span>
