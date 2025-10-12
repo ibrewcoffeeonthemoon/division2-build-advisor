68ba96145d2718d3d0c59321621c2f21c45c675e
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-12 18:47:24 +0800

    function conditional tricks works well

--- src/components/Stats/Damage/Multiplier/Table.tsx
+++ src/components/Stats/Damage/Multiplier/Table.tsx
@@ -5,12 +5,13 @@ const RowHeader = ({ text }: { text: string }) => (
   <span className="col-span-3 font-semibold">{text}</span>
 );
 
-const Cell = ({ amps }: { amps: (number | null)[] }) => {
+const Cell = ({ amps }: { amps: (() => number | null)[] }) => {
   const format = (x: number | null | undefined) => x?.toFixed(2);
 
   let text = "1";
   amps.forEach((amp) => {
-    text += amp ? ` + ${format(amp)}` : "";
+    const val = amp();
+    text += val ? ` + ${format(val)}` : "";
   });
 
   return <span className="col-span-9">{text}</span>;
@@ -33,36 +34,38 @@ export const Table = ({
     ready && (
       <div className="w-full grid grid-cols-12">
         <RowHeader text="WD" />
-        <Cell amps={[m.WDCore, m.WDType, m.WDTalent]} />
+        <Cell amps={[() => m.WDCore, () => m.WDType, () => m.WDTalent]} />
 
         <RowHeader text="TWD" />
-        <Cell amps={[m.TWD]} />
+        <Cell amps={[() => m.TWD]} />
 
         <RowHeader text="AMP1" />
-        <Cell amps={[m.AMP1]} />
+        <Cell amps={[() => m.AMP1]} />
 
         <RowHeader text="AMP2" />
-        <Cell amps={[m.AMP2]} />
+        <Cell amps={[() => m.AMP2]} />
 
         <RowHeader text="AMP3" />
-        <Cell amps={[m.AMP3]} />
+        <Cell amps={[() => m.AMP3]} />
 
         <RowHeader text="CHD/HS" />
-        <span className="col-span-9">
-          1{n0 === "critical" && m.CHD && `+ ${f(m.CHD)}`}
-          {n1 === "headshot" && m.HS && `+ ${f(m.HS)}`}
-        </span>
+        <Cell
+          amps={[
+            () => (n0 === "critical" ? m.CHD : null),
+            () => (n1 === "headshot" ? m.HS : null),
+          ]}
+        />
 
         <RowHeader text="DTA/DTH" />
-        <span className="col-span-9">
-          1{n2 === "health" && m.DTH && `+ ${f(m.DTH)}`}
-          {n2 === "armor" && m.DTA && `+ ${f(m.DTA)}`}
-        </span>
+        <Cell
+          amps={[
+            () => (n2 === "health" ? m.DTH : null),
+            () => (n2 === "armor" ? m.DTA : null),
+          ]}
+        />
 
         <RowHeader text="DTTOOC" />
-        <span className="col-span-9">
-          1{n3 === "nocover" && m.DTTOOC && `+ ${f(m.DTTOOC)}`}
-        </span>
+        <Cell amps={[() => (n3 === "nocover" ? m.DTTOOC : null)]} />
       </div>
     )
   );
