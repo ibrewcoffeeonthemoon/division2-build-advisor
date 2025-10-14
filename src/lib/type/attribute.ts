0601203a11f6451f694a59bd302620745cd92a6d
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-14 13:02:41 +0800

    all fields mandatory

--- src/lib/type/attribute.ts
+++ src/lib/type/attribute.ts
@@ -6,10 +6,10 @@ export type AttributeType = (typeof ATTRIBUTE_TYPES)[number];
 export type AttributeName = (typeof ATTRIBUTE_NAMES)[number];
 
 export type Attribute = {
-  type?: AttributeType;
+  type: AttributeType;
   name: AttributeName;
   value: number | null;
   uptime: number | null;
-  note?: string;
+  note: string;
   amplifier: Amplifier;
 };
