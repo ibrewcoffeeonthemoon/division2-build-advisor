7c1f5d2671fdc0d76b1d1cf2d27c27619c55225a
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-14 11:53:29 +0800

    better logic and format

--- src/components/Edit/lib/ItemCard/Summary.tsx
+++ src/components/Edit/lib/ItemCard/Summary.tsx
@@ -22,41 +22,42 @@ export const Summary = ({ attributes }: SummaryProps) => {
 
   return (
     <div className="grid grid-cols-24 text-info font-light items-center text-nowrap">
-      {attributes.map(({ type, name, value, uptime, note }, i) => (
-        <Fragment key={i}>
-          <span className="col-span-1 col-start-1 w-5 h-5">
-            {type && icons[type]}
-          </span>
-          <span className="col-span-3 pl-2">
-            {value && round(value * 100, 2)}%
-          </span>
-          {note === "" ? (
-            <span className="col-span-16 pl-2 text-nowrap overflow-clip overflow-ellipsis">
+      {attributes.map(({ type, name, value, uptime, note }, i) => {
+        const showNote = note && note !== "";
+        const showUptime = typeof uptime === "number" && uptime < 1;
+
+        return (
+          <Fragment key={i}>
+            <span className="col-span-1 col-start-1 w-5 h-5">
+              {type && icons[type]}
+            </span>
+            <span className="col-span-3 pl-2">
+              {value && round(value * 100, 2)}%
+            </span>
+            <span
+              className={`${
+                showNote ? "col-span-10" : "col-span-16"
+              } pl-2 text-nowrap overflow-clip overflow-ellipsis`}
+            >
               {name}
             </span>
-          ) : (
-            <>
-              <span className="col-span-10 pl-2 text-nowrap overflow-clip overflow-ellipsis">
-                {name}
+            {showNote && (
+              <span
+                className={`${
+                  showUptime ? "col-span-6" : "col-span-9"
+                } pl-2 text-nowrap overflow-clip overflow-ellipsis`}
+              >
+                {note}
               </span>
-              {uptime !== 1 ? (
-                <>
-                  <span className="col-span-6 pl-2 text-nowrap overflow-clip overflow-ellipsis">
-                    {note}
-                  </span>
-                  <span className="col-span-3 pl-2 text-base-content/50">
-                    {uptime && round(uptime * 100, 0)}%
-                  </span>
-                </>
-              ) : (
-                <span className="col-span-9 pl-2 text-nowrap overflow-clip overflow-ellipsis">
-                  {note}
-                </span>
-              )}
-            </>
-          )}
-        </Fragment>
-      ))}
+            )}
+            {showUptime && (
+              <span className="col-span-3 pl-2 text-base-content/50">
+                {uptime && round(uptime * 100, 0)}%
+              </span>
+            )}
+          </Fragment>
+        );
+      })}
     </div>
   );
 };
